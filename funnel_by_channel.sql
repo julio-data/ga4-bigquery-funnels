4ba78d3c7afa14a40e3a550088136cb98a68d2e6
Julio Sagasta <juliosagasta8@gmail.com>
2025-07-11 11:50:12 +0200

    Create funnel_by_channel.sql
    This query builds a conversion funnel segmented by traffic source (channel) using GA4 data in BigQuery.
    It calculates the number of unique users who reached each step of the funnel (from session_start to purchase), and computes step-by-step and total conversion rates per channel

---
+++ funnel_by_channel.sql
@@ -0,0 +1,29 @@
+-- Step 1: Aggregate daily unique users per funnel step
+with main_query as(
+select
+PARSE_DATE('%Y%m%d', event_date) AS period,
+traffic_source.source as channel,
+count(distinct if(event_name='session_start',user_pseudo_id,null))as session_start,
+count(distinct if(event_name='view_item',user_pseudo_id,null))as view_item,
+count(distinct if(event_name='add_to_cart',user_pseudo_id,null))as add_to_cart,
+count(distinct if(event_name='begin_checkout',user_pseudo_id,null))as begin_checkout,
+count(distinct if(event_name='purchase',user_pseudo_id,null))as purchase
+from bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20210131
+group by event_date, traffic_source.source
+)
+
+-- Step 2: Calculate conversion rates between funnel steps
+select
+channel,
+session_start,
+concat(round((view_item/session_start)*100,2),'%') as step_0_view_item,
+view_item,
+concat(round((add_to_cart/view_item)*100,2),'%') as step_1_add_to_cart,
+add_to_cart,
+concat(round((begin_checkout/add_to_cart)*100,2),'%') as step_2_begin_checkout,
+begin_checkout,
+concat(round((purchase/begin_checkout)*100,2),'%') as step_3_purchase,
+purchase,
+concat(round((purchase/session_start)*100,2),'%') as total_CR
+from main_query
+where period between  '2021-01-31' AND '2021-01-31'
