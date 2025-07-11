8abb9ec4dd4036e241ebe979f72cc1330ea1ce4a
Julio Sagasta <juliosagasta8@gmail.com>
2025-07-11 12:04:58 +0200

    Create funnel_full_dimensions.sql

---
+++ funnel_full_dimensions.sql
@@ -0,0 +1,32 @@
+-- Step 1: Aggregate daily unique users per funnel step
+with main_query as(
+select
+PARSE_DATE('%Y%m%d', event_date) AS period,
+traffic_source.source as channel,
+device.category,
+count(distinct if(event_name='session_start',user_pseudo_id,null))as session_start,
+count(distinct if(event_name='view_item',user_pseudo_id,null))as view_item,
+count(distinct if(event_name='add_to_cart',user_pseudo_id,null))as add_to_cart,
+count(distinct if(event_name='begin_checkout',user_pseudo_id,null))as begin_checkout,
+count(distinct if(event_name='purchase',user_pseudo_id,null))as purchase
+from bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_20210131
+group by event_date,traffic_source.source,device.category
+)
+
+-- Step 2: Calculate conversion rates between funnel steps
+select
+period,
+channel,
+category,
+session_start,
+concat(round(SAFE_DIVIDE(view_item,session_start)*100,2),'%') as step_0_view_item,
+view_item,
+concat(round(SAFE_DIVIDE(add_to_cart,view_item)*100,2),'%') as step_1_add_to_cart,
+add_to_cart,
+concat(round(SAFE_DIVIDE(begin_checkout,add_to_cart)*100,2),'%') as step_2_begin_checkout,
+begin_checkout,
+concat(round(SAFE_DIVIDE(purchase,begin_checkout)*100,2),'%') as step_3_purchase,
+purchase,
+concat(round(SAFE_DIVIDE(purchase,session_start)*100,2),'%') as total_CR
+from main_query
+order by period
